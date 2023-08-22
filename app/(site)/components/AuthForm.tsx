803fd6e3efb3d451eb6d7d2c5d203f0345eafb87
codepanther <pezechigozirim@gmail.com>
2023-08-22 10:16:11 +0100

    Add react-hot-toast for success and error notifications

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import { BsGithub, BsGoogle } from 'react-icons/bs';
 import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
 import AuthSocialButton from './AuthSocialButton';
+import { toast } from 'react-hot-toast';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -37,6 +38,9 @@ const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       axios.post('/api/register', data)
+        .then(() => toast.success('Success.'))
+        .catch(() => toast.error('Something bad happened.'))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === 'LOGIN') {
